7feb47f6e409cf7eb1c54919c67fe206a5c9619c
Kimberly Luke <kimberly_luke@cable.comcast.com>
2021-07-13 17:40:33 -0400

    Finished minus bonus

--- src/main/java/org/launchcode/spaday/controllers/SpaDayController.java
+++ src/main/java/org/launchcode/spaday/controllers/SpaDayController.java
@@ -49,7 +49,9 @@ public class SpaDayController {
 
     @PostMapping(value="")
     public String spaMenu(@RequestParam String name, @RequestParam String skintype, @RequestParam String manipedi, Model model) {
-
+        model.addAttribute("name", name);
+        model.addAttribute("skintype", skintype);
+        model.addAttribute("manipedi", manipedi);
         ArrayList<String> facials = new ArrayList<>();
         facials.add("Microdermabrasion");
         facials.add("Hydrofacial");
@@ -57,6 +59,7 @@ public class SpaDayController {
         facials.add("Enzyme Peel");
 
         ArrayList<String> appropriateFacials = new ArrayList<>();
+        model.addAttribute("appropriateFacials", appropriateFacials);
         for (int i = 0; i < facials.size(); i ++) {
             if (checkSkinType(skintype,facials.get(i))) {
                 appropriateFacials.add(facials.get(i));
